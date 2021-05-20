b4f2c552d10723ff2387eb8a8a45660bd461d1b6
milim203 <kmj45472510@gmail.com>
2021-05-20 16:02:53 +0900

    0520_Commit

--- 20210519_알고리즘_DFS_BFS_001/01. 바이러스 - solved.ac - class3/solution_ML(test).py
+++ 20210519_알고리즘_DFS_BFS_001/01. 바이러스 - solved.ac - class3/solution_ML(test).py
@@ -1,68 +1,37 @@
-#=================================#
-# My answer
-#=================================#
-def solution(numbers, target):
-    from itertools import product
-    #---------------------------------------------------------------#
-    # product : cartesian product
-    # - ['12','45','78'] => ['147', '148', ...]
-    # - generator이므로 무조건 list로 반환하여 저장해야 함
-    # - itertools에는 permutaions(list, r), combinations(list, r) 있음
-    #---------------------------------------------------------------#
-    answer = 0
-    numbers_choice = []
-    for i in range(len(numbers)) :
-        numbers_choice.append((numbers[i], -numbers[i]))
+#---------------------------#
+# 각 숫자별로 연결되는 숫자를 연결
+# - computer_cnt : 숫자 개수 (7)
+# - pair_cnt : 쌍의 개수     (6)
+#---------------------------#
+computer_cnt = int(input())
+pair_cnt = int(input())
+graph_list = []
+for i in range(computer_cnt + 1):
+    graph_list.append([])
+for _ in range(pair_cnt) :
+    start, end = map(int, input().strip().split())
+    graph_list[start].append(end)
+    graph_list[end].append(start)
     
-    product_comb = list(product(*numbers_choice))
     
-    for comb in product_comb :
-        if sum(comb) == target :
-            answer += 1
-    return answer
+from collections import deque
 
-#=================================#
-# Best answer1
-#=================================#
-from itertools import product
-def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
-
-#=================================#
-# Best answer2 (DFS 이용)
-#=================================#
-def dfs(numbers, idx, value, target):
-    ret = 0
-    if idx == len(numbers):
-        if value == target: return 1
-        else: return 0
-    ret += dfs(numbers, idx+1, value+numbers[idx], target)
-    ret += dfs(numbers, idx+1, value-numbers[idx], target)
-    return ret
-
-def solution(numbers, target):
-    answer = dfs(numbers, 0, 0, target)
-    return answer
+def bfs(graph_list) :
+    #-------------------------#
+    # 데크(deque) : 양방향 큐
+    #-------------------------#
+    check_que = deque([1])
+    visited = []
+    
+    while check_que :
+        check_num = check_que.popleft()
+        if check_num not in visited :
+            visited.append(check_num)
+            check_que.extend(graph_list[check_num])
+        print(check_num, visited, check_que)
+    return visited
 
 
-#=================================#
-# Best answer3 (DFS 이용)
-#=================================#
-answer = 0
-def DFS(idx, numbers, target, value):
-    global answer
-    N = len(numbers)
-    if(idx== N and target == value):
-        answer += 1
-        return
-    if(idx == N):
-        return
-
-    DFS(idx+1,numbers,target,value+numbers[idx])
-    DFS(idx+1,numbers,target,value-numbers[idx])
-def solution(numbers, target):
-    global answer
-    DFS(0,numbers,target,0)
-    return answer
+    
+result = bfs(graph_list)
+print(len(result)-1)
