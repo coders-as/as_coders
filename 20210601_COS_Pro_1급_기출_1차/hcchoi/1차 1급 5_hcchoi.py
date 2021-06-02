eef554bf39072fde5f0d772d41f2db77840be942
swab2000 <chlgudcjfsla@naver.com>
2021-06-02 13:28:32 +0900

    hcchoi_20210602_001 (#32)
    * hcchoi_20210512_001
    
    * hcchoi_20210520_001
    
    * hcchoi_20210602_001
    
    Co-authored-by: unknown <ddchoi@lgcns.com>

---
+++ 20210601_COS_Pro_1급_기출_1차/hcchoi/1차 1급 5_hcchoi.py
@@ -0,0 +1,31 @@
+#You may use import as below.
+#import math
+
+def solution(n):
+    # Write code here.
+    answer = 0
+    
+    # graph = {(0,0):(0,1), (0,1):(0,2), (0,2):(1,2), (1,2):(2,2)}
+    # mat = dict()
+    
+    # for i in range(n):
+    #     for j in range(n):
+            
+    
+    return answer
+
+
+#The following is code to output testcase.
+n1 = 3
+ret1 = solution(n1)
+
+#Press Run button to receive output. 
+print("Solution: return value of the function is", ret1, ".")
+    
+n2 = 2
+ret2 = solution(n2)
+
+#Press Run button to receive output. 
+print("Solution: return value of the function is", ret2, ".")
+
+
